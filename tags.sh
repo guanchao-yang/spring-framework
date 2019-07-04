959e83420317922e24aa1f2557ef93b5b37b86ce
雪连 <guanchao.ygc@antfin.com>
2019-07-04 17:25:57 +0800

    add tag

---
+++ tags.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Repo: someuser/myframework
+# Fork: superteam/myframework
+
+# Track:
+#git clone git@github.com:guanchao-yang/spring-boot.git
+#cd spring-boot
+git remote add upstream git@github.com:spring-projects/spring-framework.git
+
+# Update:
+git fetch upstream
+git rebase upstream/master
+git push
+git push --tags
